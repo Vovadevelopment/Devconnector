3168612be2c465c649af23e8cee29e8986ad499d
Kredynets Volodymyr <vovadevelopment@gmail.com>
2020-07-07 01:13:13 +0300

    add auth and profile model

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,17 +3,14 @@ const config = require('config');
 
 module.exports = function (req, res, next) {
 	const token = req.header('x-auth-token');
-	console.log('token', token);
 	if (!token) {
 		return res.status(401).json({'msg': 'No token, authorization denied'});
 	}
 	try {
 		const decoded = jwt.verify(token, config.get('jwtSecret'));
-		console.log('decoded', decoded);
 		req.user = decoded.user;
-		next()
+		next();
 	} catch(err) {
-		console.log(err);
 		res.status(401).json({mgs: 'Token is not valid'});
 	}
 };
