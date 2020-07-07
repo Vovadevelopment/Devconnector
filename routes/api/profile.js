5c841d36e24b460a0a9b25df1786d2115de36c2c
Kredynets Volodymyr <vovadevelopment@gmail.com>
2020-07-07 23:09:44 +0300

    working on profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -66,6 +66,42 @@ router.post('/', [auth, [
 		status,
 		githubusername
 	};
+
+	const socialfields = { youtube, twitter, instagram, linkedin, facebook };
+	for (const [key, value] of Object.entries(socialfields)) {
+		if (value && value.length > 0)
+			socialfields[key] = normalize(value, { forceHttps: true });
+	}
+	profileFields.social = socialfields;
+
+	try {
+		// Using upsert option (creates new doc if no match is found):
+		let profile = await Profile.findOneAndUpdate(
+			{ user: req.user.id },
+			{ $set: profileFields },
+			{ new: true, upsert: true }
+		);
+		res.json(profile);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+
 });
+
+// @route GET api/profile/user/:user_id
+// @desc Get profile by user ID
+// @access Public
+
+router.get('/', async (req, res) => {
+	try {
+		const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+		res.json(profiles);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
 
 module.exports = router;
